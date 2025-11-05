d387233a7f8b16e907b0a937cdf698df950d806e
とあとも <toatomo2913@gmail.com>
2025-11-05 19:30:01 +0900

    Todoセッションとバリデーション実装済み

---
+++ src/app/Http/Controllers/TodoController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TodoRequest;
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    public function index()
+    {
+        $todos = Todo::all();
+        return view('index', compact('todos'));
+    }
+
+    public function store(TodoRequest $request)
+    {
+        //フォームから送信された[content]を取得してDBに保存する
+        $todo = $request->only(['content']);
+        Todo::create($todo);
+
+        return redirect('/')->with('message', 'Todoを作成しました');
+    }
+}
