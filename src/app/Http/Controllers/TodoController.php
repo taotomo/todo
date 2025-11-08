573586dd149370576f464a827c12bd23ef0a3ea3
とあとも <toatomo2913@gmail.com>
2025-11-08 14:18:20 +0900

    Todoの更新・削除機能を実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -11,15 +11,29 @@ class TodoController extends Controller
     public function index()
     {
         $todos = Todo::all();
+
         return view('index', compact('todos'));
     }
 
     public function store(TodoRequest $request)
     {
-        //フォームから送信された[content]を取得してDBに保存する
         $todo = $request->only(['content']);
         Todo::create($todo);
 
         return redirect('/')->with('message', 'Todoを作成しました');
+    }
+
+    public function update(TodoRequest $request)
+    {
+        $todo = $request->only(['content']);
+        Todo::find($request->id)->update($todo);
+
+        return redirect('/')->with('message', 'Todoを更新しました');
+    }
+    
+    public function destroy(Request $request)
+    {
+        Todo::find($request->id)->delete();
+        return redirect('/')->with('message', 'Todoを削除しました');
     }
 }
--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -6,9 +6,20 @@
 
 @section('content')
 <div class="todo__alert">
+  @if(session('message'))
   <div class="todo__alert--success">
-    Todoを作成しました
+    {{ session('message') }}
   </div>
+  @endif
+  @if ($errors->any())
+  <div class="todo__alert--danger">
+    <ul>
+      @foreach ($errors->all() as $error)
+      <li>{{ $error }}</li>
+      @endforeach
+    </ul>
+  </div>
+  @endif
 </div>
 
 <div class="todo__content">
@@ -29,9 +40,12 @@
       @foreach ($todos as $todo)
       <tr class="todo-table__row">
         <td class="todo-table__item">
-          <form class="update-form">
+          <form class="update-form" action="/todos/update" method="POST">
+            @method('PATCH')
+            @csrf
             <div class="update-form__item">
-              <input class="update-form__item-input">{{ $todo['content'] }}</input>
+              <input class="update-form__item-input" type="text" name="content" value="{{ $todo['content'] }}">
+              <input type="hidden" name="id" value="{{ $todo['id'] }}">
             </div>
             <div class="update-form__button">
               <button class="update-form__button-submit" type="submit">更新</button>
@@ -39,8 +53,11 @@
           </form>
         </td>
         <td class="todo-table__item">
-          <form class="delete-form">
+          <form class="delete-form" action="/todos/delete" method="POST">
+            @method('DELETE')
+            @csrf
             <div class="delete-form__button">
+              <input type="hidden" name="id" value="{{ $todo['id'] }}">
               <button class="delete-form__button-submit" type="submit">削除</button>
             </div>
           </form>
@@ -48,23 +65,6 @@
       </tr>
       @endforeach
     </table>
-    <div class="todo__alert">
-        @if(session('message'))
-        <div class="todo__alert--success">
-            Todoを作成しました
-            {{ session('message') }}
-        </div>
-        @endif
-        @if ($errors->any())
-        <div class="todo__alert--danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-        @endif
-    </div>
   </div>
 </div>
 @endsection
